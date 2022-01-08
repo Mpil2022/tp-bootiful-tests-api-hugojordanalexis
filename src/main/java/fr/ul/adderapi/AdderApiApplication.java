f4709786a677432a353a90a4bbc7125f42a99589
liaud1u <alexis.liaud7@etu.univ-lorraine.fr>
2022-01-08 09:20:42 +0100

    Init

---
+++ src/main/java/fr/ul/adderapi/AdderApiApplication.java
@@ -0,0 +1,13 @@
+package fr.ul.adderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ul/adderapi/AdderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ul.adderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
